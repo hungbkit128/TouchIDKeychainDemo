2909d5dd2a97715322627a52d94ea5184568bd79
Hưng Trần <hungbkit128@gmail.com>
2018-08-21 10:11:13 +0700

    cm test

--- TouchIDKeychainDemo/TouchIDKeychainDemo/ViewController.swift
+++ TouchIDKeychainDemo/TouchIDKeychainDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("Hello Hung")
+        print("Hello Hung")
     }
 
     override func didReceiveMemoryWarning() {
