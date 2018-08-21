d053ceea5480b945b77b26482f210070dc901686
Hưng Trần <hungbkit128@gmail.com>
2018-08-21 09:48:05 +0700

    cm code new

---
+++ TouchIDKeychainDemo/TouchIDKeychainDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIDKeychainDemo
+//
+//  Created by Vtsoft2 on 8/21/18.
+//  Copyright © 2018 hungtv64. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
