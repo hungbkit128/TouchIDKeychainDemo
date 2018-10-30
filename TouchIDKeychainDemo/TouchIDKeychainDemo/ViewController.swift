316b840afcb24e1a56e712e5369cb71e67b59e55
Hưng Trần <hungbkit128@gmail.com>
2018-10-30 08:50:03 +0700

    hungtv cm

--- TouchIDKeychainDemo/TouchIDKeychainDemo/ViewController.swift
+++ TouchIDKeychainDemo/TouchIDKeychainDemo/ViewController.swift
@@ -17,12 +17,45 @@ class ViewController: UIViewController {
         print("Hello Hung")
         print("Hello Hung")
     }
+    
+    @IBAction func didTouchUpInsideRegisterButton(_ sender: Any) {
+        saveAccountToKeychain(userName: userNameTextField.text, password: passwordTextField.text) { [weak self] in
+            self?.performSegue(withIdentifier: "PushToLogin", sender: nil)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    private func saveAccountToKeychain(userName: String?, password: String?, finished: (() -> ())?) {
+        guard let userName = userName?.trimmingCharacters(in: .whitespaces), !userName.isEmpty else {
+            // show error
+            return
+        }
+        guard let password = password?.trimmingCharacters(in: .whitespaces), !password.isEmpty else {
+            // show error
+            return
+        }
+        // Lưu username vào UserDefaults
+        UserDefaults.standard.set(userName, forKey: "lastAccessedUserName")
+        // Khởi tạo Keychain
+        let passwordItem = KeychainPasswordItem(
+            service: KeychainConfiguration.serviceName,
+            account: userName,
+            accessGroup: KeychainConfiguration.accessGroup
+        )
+        do {
+            // thực hiện lưu vào keychain
+            try passwordItem.savePassword(password)
+            // call back là success
+            finished?()
+        } catch {
+            // khi có lỗi
+            print("Error saving password")
+        }
+    }
 
 }
 
